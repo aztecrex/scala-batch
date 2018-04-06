8ab81cc423a982398852836dc1aacaab4766b569
Greg Wiley <gwiley@cj.com>
2018-04-06 16:14:39 -0700

    remove old batchprocessor

--- src/main/scala/com/fintech/lib/batch/BatchContext.scala
+++ src/main/scala/com/fintech/lib/batch/BatchContext.scala
@@ -23,39 +23,6 @@ case class BatchContext[SRC, INCOMPLETE]() {
 
 }
 
-
-class BatchProcessor[SRC, INCOMPLETE, +A](private val r: Iterable[SRC] => Iterable[Either[INCOMPLETE, A]]) {
-
-  def fold[B](initial: B)(f: (B, A) => B): BatchProcessor[SRC, INCOMPLETE, B] = {
-    new BatchProcessor({sources: Iterable[SRC] =>
-      val unpacked = r(sources)
-      val summary = unpacked.filter(_.isRight).map(_.right.get).foldLeft[B](initial)(f)
-      unpacked.map(x => x.right.map(Function.const(summary)))
-    })
-  }
-
-  def map[B](f: A => B): BatchProcessor[SRC, INCOMPLETE, B]
-    = new BatchProcessor({sources: Iterable[SRC] => r(sources).map(_.right.map(f))})
-
-  def flatMap[B](f: A => BatchProcessor[SRC, INCOMPLETE, _ <: B]): BatchProcessor[SRC, INCOMPLETE, B] = {
-      new BatchProcessor({sources: Iterable[SRC] =>
-        val unpacked = r(sources).zip(sources)
-        val applied = unpacked.map(p => (p._1.right.map(f), p._2))
-        val exploded = applied.map(p => p._1.right.map(bp => bp.r(Seq(p._2)).head))
-        val joined = exploded.map(e => e.joinRight)
-
-        joined
-      })
-  }
-
-  protected def exec_(batch: Iterable[SRC]): ProcessResult[SRC, INCOMPLETE, A] = {
-    ProcessResult(r(batch).zip(batch).zipWithIndex.map(p => Item(p._2, p._1._2,p._1._1 )))
-  }
-  def run(batch: Iterable[SRC]): Iterable[Item[SRC,A]] = exec_(batch).complete
-  def exec(batch: Iterable[SRC]): ProcessResult[SRC, INCOMPLETE, A] = exec_(batch)
-}
-
-
 class AltProcessor[SRC, INCOMPLETE, +A](private val runLine: Ctx[SRC] => Either[INCOMPLETE, A]) {
 
   def fold[B](initial: B)(f: (B, A) => B): AltProcessor[SRC, INCOMPLETE, B] = {
