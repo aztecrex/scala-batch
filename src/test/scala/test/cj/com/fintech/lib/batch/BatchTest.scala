387235ae1c06019ac1076f0e4491d8d12050e451
Greg Wiley <gwiley@cj.com>
2018-04-08 20:43:09 -0700

    show more weirdness - non-working checkpoint

--- src/test/scala/test/cj/com/fintech/lib/batch/BatchTest.scala
+++ src/test/scala/test/cj/com/fintech/lib/batch/BatchTest.scala
@@ -422,14 +422,19 @@ class BatchTest extends FunSuite {
 
     // given
     val context = BatchContext[Int, Symbol]
+    import context._
     val batch = Seq(1, 2, 3, 5, 4, 300)
 
     val processor = for {
-      src <- context.source()
-      x = BigDecimal(src)
-      sum <- context.pure(x).foldLeft(BigDecimal(0))((a, agg) => a + agg)
-      ans <- context.pure(x / sum)
-    } yield sum
+      src <- source().map(BigDecimal(_))
+//      sum <- source().map(BigDecimal(_)).foldLeft(BigDecimal(0))((a, agg) => a + agg)
+      sum1 <- pure(src).foldLeft(BigDecimal(0))(_ + _)
+//      sum <- pure(x).foldLeft(BigDecimal(0))((a, agg) => a + agg)
+//      ans <- pure(x / sum)
+      n <- source().map(BigDecimal(_))
+//      ans <- source().map(BigDecimal(_)).map(_ / sum)
+      ans <- pure(n / sum1)
+    } yield sum1
 
     // when
     val actual = processor.run(batch)
